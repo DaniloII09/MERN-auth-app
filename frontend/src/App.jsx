1e2820efc1c3ada0a865746f7666352d4d8119c3
DaniloII09 <00377223@uca.edu.sv>
2026-09-03 02:49:37 -0600

    fix: background color changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div
       className="min-h-screen bg-linear-to-br 
-    from-zinc-900 via-neutral-900 to-black 
+    from-zinc-900 via-amber-950 to-black 
     flex items-center justify-center relative overflow-hidden"
     >
       <FloatingShape
